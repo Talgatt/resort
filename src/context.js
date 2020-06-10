e24563e1d6a36d9ab885d4ed244e22e81c20b28c
Talgatt <talgat001@gmail.com>
2020-06-10 16:14:44 -0700

    first commit

---
+++ src/context.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import items from "./data";
+const RoomContext = React.createContext();
+// <RoomContext.Provider value={}
+
+export default class RoomProvider extends Component {
+  state = {
+    greeting: "hello",
+    name: "john",
+  };
+
+  render() {
+    return (
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
