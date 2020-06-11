8982b6af7aa90f4bd97646ba39dfeb3aa85d3446
Talgatt <talgat001@gmail.com>
2020-06-11 15:49:36 -0700

    added roomslist and rooms components

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import RoomsFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <RoomsFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomsFilter from "./RoomFilter";
+// import RoomList from "./RoomList";
+// import { RoomConsumer } from "../context";
+// import Loading from "./Loading";
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         console.log(value);
+//         return (
+//           <div>
+//             hello from rooms container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
